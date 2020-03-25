e7fdb078e1ec6768accbc32a25e90fc5ecb2e2bf
marcelosena <marcelosena19@gmail.com>
2020-03-25 01:47:28 -0300

    qqq

--- _site.R
+++ _site.R
@@ -2,10 +2,15 @@
 suppressPackageStartupMessages({
   library(tidyverse)
   library(stringr)
+  library(plotly)
 })
 
 ## variables I need for my site 
+y0 = 542
 data <- readr::read_csv('important_data.csv')
+data_weak = read.table(paste0("C:/Users/Marcelo/Desktop/Covid/primeira_projs/estado-rio-weak-",as.character(y0),".csv"),sep = '\t', header = TRUE)
+data_mod = read.table(paste0("C:/Users/Marcelo/Desktop/Covid/primeira_projs/estado-rio-moderate-",as.character(y0),".csv"),sep = '\t', header = TRUE)
+data_strong = read.table(paste0("C:/Users/Marcelo/Desktop/Covid/primeira_projs/estado-rio-strong-",as.character(y0),".csv"),sep = '\t', header = TRUE)
 
 
 ## knitr options I want set as default for all ('global') code chunks
